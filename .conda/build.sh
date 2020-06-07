8582e7d6f2ba1fdd264d1c79ed604ad861651188
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-07 20:48:46 +0200

    bugfix(circleci) Fixed tmp/test-reports folder creation.

--- .conda/build.sh
+++ .conda/build.sh
@@ -19,7 +19,7 @@ make -j2
 ctest -T Test
 
 # Extract test.xml results to $HOME/tmp/test-reports
-mkdir $HOME/tmp/test-reports
+mkdir -p $HOME/tmp/test-reports
 find ./Testing -name \*.xml -exec cp {} $HOME/tmp/test-reports/test.xml \;
 
 make install
